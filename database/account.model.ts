1bde5724ca7aba1951b1877c22c9e81cd78180ba
Rohan556 <rohanmc9184@gmail.com>
2025-02-07 09:31:22 +0530

    Implement account model

---
+++ database/account.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema, models } from "mongoose";
+
+export interface IAccount {
+  userId: string;
+  name: string;
+  image: string;
+  password: string;
+  provider: string;
+  providerAccountId: string;
+}
+
+const AccountSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  name: { type: String, required: true },
+  image: { type: String },
+  password: { type: String },
+  provider: { type: String, required: true },
+  providerAccountId: { type: String, required: true },
+});
+
+const Account = models.Account ?? model<IAccount>("Account", AccountSchema);
+
+export default Account;
